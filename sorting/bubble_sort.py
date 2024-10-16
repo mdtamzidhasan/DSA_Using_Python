63d4c08c55163fdec7600973a442f69b4472de18
mdtamzidhasan <tamzidhasan2020@gmail.com>
2024-10-16 20:01:45 +0600

    improved bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -9,3 +9,20 @@ for i in range(n-1):
 
 print('sorted array: ',my_array)
 
+#Bubble sort improved
+#If it is already sorted, then we don't need to use any algorithm.
+
+n = len(my_array)
+for i in range(n-1):
+    swaped = False
+    for j in range(n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j],my_array[j+1] = my_array[j+1],my_array[j]
+            swaped = True
+    if not swaped:
+        break
+
+
+print('sorted array: ',my_array)
+
+#the time complexity for Bubble Sort is: O(n)^2
---
+++ sorting/selection_sort.py
@@ -0,0 +1,2 @@
+#The Selection Sort algorithm finds the lowest value in an array and moves it to the front of the array.
+
